577bf8692e6fe44362e4a5410a01a0927aa1264c
rishabh-90 <rihabhaggarwal@outlook.com>
2021-03-14 19:46:22 -0300

    Updated Recruitment Page

--- src/pages/recruitment.js
+++ src/pages/recruitment.js
@@ -1,6 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import data from "../../content/recruitment.json"
+import thumb from "../../static/img/logo-4.svg"
+import upload from "../../static/img/upload.png"
+import titleImage from "../../static/img/shape-polygon.svg"
 
 const Recruitment = () => {
   return (
@@ -8,8 +12,8 @@ const Recruitment = () => {
       <div className="content-wrap">
         <div className="recruitment-wrap section-padding pb-0">
           <div className="section-title v4">
-            <img src="assets/img/shape-polygon.svg" alt="Image" />
-            <h2>Recruitment</h2>
+            <img src={titleImage} alt="Image" />
+            <h2>{data.title}</h2>
           </div>
         </div>
         <div className="recruit-hero">
@@ -18,9 +22,10 @@ const Recruitment = () => {
               <div className="col-lg-10 left_md">
                 <div className="recruit-hero-content">
                   <h2>
-                    Try, Fail, Iterate! <span>の精神</span>
+                    {data.subheading.engtext}{" "}
+                    <span>{data.subheading.japtext}</span>
                   </h2>
-                  <p>熱い想いをもった仲間を募集しています！</p>
+                  <p>{data.subheading.description}</p>
                 </div>
               </div>
             </div>
@@ -35,34 +40,18 @@ const Recruitment = () => {
                   <p>共感いただける方、一緒に働きましょう！</p>
                 </div>
               </div>
-
-              <div className="col-lg-4">
-                <div className="quality-item">
-                  <span>#1</span>
-                  <h4>Be Passionate</h4>
-                  <h5>パッションに生きる</h5>
-                  <p>
-                    自分の中の「熱量」を大事にし、
-                    それに向かって貪欲に成長し続けよう。
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="quality-item">
-                  <span>#2</span>
-                  <h4>Be Unique</h4>
-                  <h5>唯一無二の存在であれ</h5>
-                  <p>となりの人とは違う自分だけの強みを 磨き続けよう。</p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="quality-item">
-                  <span>#3</span>
-                  <h4>Be Challenging</h4>
-                  <h5>まずは、やってみよう</h5>
-                  <p>やってみて失敗したら、何か学んで、 また次やってみよう。</p>
-                </div>
-              </div>
+              {data.corevalues.map((value, index) => {
+                return (
+                  <div className="col-lg-4" key={index}>
+                    <div className="quality-item">
+                      <span>#{index + 1}</span>
+                      <h4>{value.heading}</h4>
+                      <h5>{value.japheading}</h5>
+                      <p>{value.description}</p>
+                    </div>
+                  </div>
+                )
+              })}
             </div>
           </div>
         </div>
@@ -70,95 +59,35 @@ const Recruitment = () => {
           <div className="row">
             <div className="col-md-12 left_md">
               <div className="section-title v4 style1">
-                <img src="assets/img/logo-4.svg" alt="Image" />
+                <img src={thumb} alt="Image" />
                 <h2>の５いいね！</h2>
               </div>
             </div>
 
-            <div className="col-lg-4">
-              <div className="simple-item">
-                <div className="icon">
-                  <img src="assets/img/thumb.svg" alt="Image" />
-                  <span className="number">1</span>
-                </div>
+            {data.fivelikes.map((like, index) => {
+              return (
+                <div className="col-lg-4" key={index}>
+                  <div className="simple-item">
+                    <div className="icon">
+                      <img src={like.image} alt="Image" />
+                      <span className="number">{index + 1}</span>
+                    </div>
 
-                <h4 className="thumb_title">人間関係</h4>
-                <p>
-                  オープンマインドで、どんどん意見 <br />
-                  を出し合える関係です！
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="simple-item">
-                <div className="icon">
-                  <img src="assets/img/thumb.svg" alt="Image" />
-                  <span className="number">2</span>
+                    <div dangerouslySetInnerHTML={{ __html: like.heading }} />
+                    <div
+                      dangerouslySetInnerHTML={{ __html: like.description }}
+                    />
+                  </div>
                 </div>
-                <h4>
-                  フレキシブルな <br />
-                  勤務時間
-                </h4>
-                <p>
-                  「自由と責任は一対」という <br />
-                  スウェーデンのマインドを取り入 <br />
-                  れています。
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="simple-item">
-                <div className="icon">
-                  <img src="assets/img/thumb.svg" alt="Image" />
-                  <span className="number">3</span>
-                </div>
-                <h4>
-                  なんか <br />
-                  インターナショナル
-                </h4>
-                <p>
-                  北欧出身のCEOに加え、海外経験の <br />
-                  あるメンバーが複数。 <br />
-                  各国の情報や英語が飛び交う。
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="simple-item">
-                <div className="icon">
-                  <img src="assets/img/thumb.svg" alt="Image" />
-                  <span className="number">4</span>
-                </div>
-                <h4>モダンなスキル選定</h4>
-                <p>
-                  主に、React, Golang, Flutterを <br />
-                  用いて開発。基本的にグローバル <br />
-                  水準のスキルを扱う。
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="simple-item">
-                <div className="icon">
-                  <img src="assets/img/thumb.svg" alt="Image" />
-                  <span className="number">5</span>
-                </div>
-                <h4>香川</h4>
-                <p>
-                  田舎すぎず、都会すぎず。オフィス <br />
-                  のある高松は車がなくても住める <br />
-                  コンパクトシティ。郊外へ少し出 <br />
-                  ると、山や田んぼが広がる地域へ。
-                </p>
-              </div>
-            </div>
+              )
+            })}
           </div>
         </div>
         <div className="apply-button section-padding">
           <h2>JOBS</h2>
           <p>応募はコチラから！</p>
-          <a href="#" className="btn v3">
-            Wantedly <img src="assets/img/upload.png" alt="Image" />
+          <a href={data.joblink} className="btn v3">
+            Wantedly <img src={upload} alt="Image" />
           </a>
         </div>
       </div>
